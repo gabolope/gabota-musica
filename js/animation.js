03a2b7951dd661c3a965bd3ef76d7d21720cb77a
Gabriel López <gabi1517@hotmail.com>
2021-07-29 13:25:40 -0300

    mejora de estetica de footer, animaciones, botones y agregado de fondo de contacto

--- js/animation.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-    $('a[href*=#header]').on('click', function(e) {
-      e.preventDefault();
-      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
-    });
-  });
